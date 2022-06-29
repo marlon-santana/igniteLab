c38117c767b7eea0c0b1deb1b636e0038b810f0d
Marlon Oliveira <marlon.oliveira@somagrupo.com.br>
2022-06-28 21:14:36 -0300

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,15 @@
-import { gql, useQuery } from '@apollo/client';
 
 
-const GET_LESSONS_QUERY = gql `
-query {
-  lessons {
-    id,
-    title
-  }
-}
-`
 interface lessons {
   id: String;
   title:String;
 }
 
 function App() {
-  const  {data } = useQuery<{lessons: lessons[]}>(GET_LESSONS_QUERY)
+  
 
   return (
-    <ul>
-      {data?.lessons.map(lesson => {
-        return <li>{lesson.title}</li>
-      })}
-    </ul>
+    <h1>HELLO WORD</h1>
   )
 }
 
